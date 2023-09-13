6df5a22b7724f2f8ce41053bafc0d8795a512566
Brian Merriam <merriam@gmail.com>
2023-09-12 21:13:25 -0400

    updated to 1.20.1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Minecraft Seed Helper
+# Minecraft Inventory Tracker
+
+## Goals
+
+* Have every item a player uses be posted to an API
+* Users can login to a website with Microsoft SSO
+* Users can associate their player data to their Users
+* Statistics are shown about block usage
+
+tables:
--- src/main/java/com/targetedentropy/inventorytracker/inventorytracker.java
+++ src/main/java/com/targetedentropy/inventorytracker/inventorytracker.java
@@ -1,11 +1,7 @@
-package com.example.seedhelper;
-
-
-import com.example.seedhelper.SeedCommand;
+package com.targetedentropy.inventorytracker;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
@@ -18,22 +14,20 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
-import java.util.List;
+
+
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(SeedHelper.MODID)
-public class SeedHelper
+@Mod(inventorytracker.MODID)
+public class inventorytracker
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "seedhelper";
+    public static final String MODID = "inventorytracker";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public SeedHelper()
+    public inventorytracker()
     {
-
-        MinecraftForge.EVENT_BUS.addListener(SeedCommand::commandRegister);
-
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register ourselves for server and other game events we are interested in
@@ -41,32 +35,9 @@ public class SeedHelper
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-
-        for (Item item : ForgeRegistries.ITEMS.getValues()) {
-            ResourceLocation itemName = ForgeRegistries.ITEMS.getKey(item);
-            assert itemName != null;
-            if (itemName.getPath().contains("seed")) {
-                LOGGER.info("Seed: {}", itemName);
-            }
-
-            if (itemName.getPath().contains("chicken")) {
-                LOGGER.info("Chicken: {}", itemName);
-            }
-
-        }
-
-        List<ItemStack> ALL_SEEDS = ForgeRegistries.ITEMS.getValues().stream().map(item -> {
-
-            ResourceLocation itemName = ForgeRegistries.ITEMS.getKey(item);
-            assert itemName != null;
-            if (itemName.getPath().contains("seed")) {
-                return new ItemStack(item);
-            }
-
-            return ItemStack.EMPTY;
-        }).filter(stack -> !stack.isEmpty()).toList();
-
     }
+
+
 
     @SubscribeEvent
     public void placeItem(PlayerInteractEvent.RightClickBlock event)
@@ -78,5 +49,4 @@ public class SeedHelper
         LOGGER.info("Item placed: {}", itemName);
 
     }
-
 }
