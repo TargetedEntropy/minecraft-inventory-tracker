a628cdd33ba54259eb27597659bf20b0b3cb4c52
Brian Merriam <merriam@gmail.com>
2023-10-18 17:14:03 -0400

    update readme and command poc

--- src/main/java/com/targetedentropy/inventorytracker/inventorytracker.java
+++ src/main/java/com/targetedentropy/inventorytracker/inventorytracker.java
@@ -20,16 +20,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.Command;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.commands.arguments.EntityArgument;
 
-import static java.lang.Boolean.TRUE;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(inventorytracker.MODID)
@@ -39,6 +58,9 @@ public class inventorytracker
     public static final String MODID = "inventorytracker";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
+    // Store the Http endpoint
+    private static String http_url = "http://main.tovdc.com:5050/items";
+
 
     public inventorytracker()
     {
@@ -51,6 +73,7 @@ public class inventorytracker
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
+
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
             dispatcher.register(Commands.literal("purge")
                     .executes(context -> {
@@ -61,10 +84,31 @@ public class inventorytracker
                         }
                     })
             );
+
     }
 
+//    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+//        dispatcher.register(Commands.literal("sbitem")
+//                .requires(source -> source.hasPermission(2))
+//                .then(Commands.argument("targets", EntityArgument.players()).then(Commands.argument("item", StringArgumentType.string()).suggests(SUGGEST_SBITEM)
+//                        .executes(context -> give(StringArgumentType.getString(context, "item"), EntityArgument.getPlayer(context, "targets"), new CompoundTag()))
+//                        .then(Commands.argument("skyblockData", CompoundTagArgument.compoundTag())
+//                                .executes(context -> give(StringArgumentType.getString(context, "item"), EntityArgument.getPlayer(context, "targets"), CompoundTagArgument.getCompoundTag(context, "skyblockData")))))));
+//    }
+
+    private static int registerAlias(CommandDispatcher<CommandSourceStack> dispatcher, String alias, String command) {
+        dispatcher.register(Commands.literal(alias)
+                .executes((commandContext) -> {
+                    dispatcher.execute(command, commandContext.getSource());
+                    return Command.SINGLE_SUCCESS;
+                })
+        );
+        return Command.SINGLE_SUCCESS;
+    }
+
+
     public static int postItem(String itemName, Player player) throws IOException {
-        URL url = new URL("http://main.tovdc.com:5050/items");
+        URL url = new URL(http_url);
         HttpURLConnection con = (HttpURLConnection)url.openConnection();
 
         con.setRequestMethod("POST");
@@ -73,23 +117,14 @@ public class inventorytracker
         con.setDoOutput(true);
 
         String json = "{\"uuid\": \""+player.getUUID()+"\", \"item_name\": \""+itemName+"\"}";
-        LOGGER.info("JsonStr: {}", json);
+        LOGGER.debug("JsonStr: {}", json);
+
         // Write the body to the stream
         try(OutputStream os = con.getOutputStream()) {
             byte[] input = json.getBytes("utf-8");
             os.write(input, 0, input.length);
         }
 
-        // Read the response
-        try(BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), "utf-8"))) {
-            StringBuilder response = new StringBuilder();
-            String responseLine = null;
-            while ((responseLine = br.readLine()) != null) {
-                response.append(responseLine.trim());
-            }
-            System.out.println(response.toString());
-        }
         return 1;
     }
 
@@ -99,7 +134,7 @@ public class inventorytracker
         ResourceLocation itemName = ForgeRegistries.ITEMS.getKey(stack.getItem());
         Player player = event.getEntity();
 
-        LOGGER.info("Item placed: {}", itemName);
+        LOGGER.debug("Item placed: {}", itemName);
         postItem(String.valueOf(itemName), player);
     }
 }
